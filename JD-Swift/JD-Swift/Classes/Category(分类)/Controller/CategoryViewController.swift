70c47c931ee1190073c296fe23fcddf781b2742d
zp <zhangpinga@ctrip.com>
2017-05-09 12:29:55 +0800

    添加几大控制器。

---
+++ JD-Swift/JD-Swift/Classes/Category(分类)/Controller/CategoryViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CategoryViewController.swift
+//  JD-Swift
+//
+//  Created by zp on 17/5/9.
+//  Copyright © 2017年 zp. All rights reserved.
+//
+
+import Foundation
---
+++ JD-Swift/JD-Swift/Classes/Main(主要)/Controller/JDTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  JDTabBarController.swift
+//  JD-Swift
+//
+//  Created by zp on 17/5/9.
+//  Copyright © 2017年 zp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class JDTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    func addAllChildVcs() -> Void {
+        
+    }
+}
