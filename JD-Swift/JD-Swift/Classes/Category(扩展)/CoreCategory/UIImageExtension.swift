41846a483750613605e818fd8adb0dbe990e0459
zp <intheair100@163.com>
2017-05-13 13:05:27 +0800

    uiimiage extension

--- JD-Swift/JD-Swift/Classes/Category(扩展)/CoreCategory/UIImageExtension.swift
+++ JD-Swift/JD-Swift/Classes/Category(扩展)/CoreCategory/UIImageExtension.swift
@@ -9,7 +9,15 @@
 import Foundation
 import UIKit
 extension UIImage{
-    static func imageWithName(imageName:String) -> UIImage {
+    
+    /**
+     *  争对ios7以上的系统适配新的图片资源
+     *
+     *  @param imageName 图片名称
+     *
+     *  @return 新的图片
+     */
+     func imageWithName(imageName:String) -> UIImage {
         var newImage:UIImage? = nil
         if iOS7 {
             newImage = UIImage.init(named: imageName + "_os7")!
@@ -19,4 +27,33 @@ extension UIImage{
         }
         return newImage!
     }
+    
+    func resizableImageWithName(imagName:String) -> UIImage {
+        let image:UIImage = UIImage.init(named: imagName)!
+        // 原有图片的宽高的一半
+        let newImage:UIImage = image.resizableImage(withCapInsets: UIEdgeInsetsMake(image.size.height * 0.5, image.size.width * 0.5, image.size.height * 0.5, image.size.width * 0.5), resizingMode: UIImageResizingMode.stretch)
+        return newImage
+        
+    }
+    
+    
+    /**
+     *  实现图片的缩小或者放大
+     *
+     *  @param image 原图
+     *  @param size  大小范围
+     *
+     *  @return 新的图片
+     */
+    func scaleImageWithSize(size:CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        self.draw(in: CGRect.init(x: 0, y: 0, width: size.width, height: size.height))
+        let scaledImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return scaledImage
+        
+    }
+    
+    
+    
 }
