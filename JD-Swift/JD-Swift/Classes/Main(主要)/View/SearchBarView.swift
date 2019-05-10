7f37902f9169c6d9dc7934434f9ffa7c428e29fc
zp <zhangpinga@ctrip.com>
2019-05-10 22:55:32 +0800

    修改searchbarview

--- JD-Swift/JD-Swift/Classes/Main(主要)/View/SearchBarView.swift
+++ JD-Swift/JD-Swift/Classes/Main(主要)/View/SearchBarView.swift
@@ -12,6 +12,39 @@ class SearchBarView: UIView {
 
     var placeholder:String?
     weak var delegate:SearchBarViewDelegate?
+    var audioButton:UIButton? = nil
+    var searchButton:UIButton? = nil
+    var backgroundView:UIView? = nil
+    let kXMargin:CGFloat = 8.0
+    let kYMargin:CGFloat = 4.0
+    let kIconSize:CGFloat = 20.0
+    let kSearchBarHeight:CGFloat = 30.0
+
+
+    
+    func setDefault() -> Void {
+        let boundWith:CGFloat = self.bounds.size.width
+        let boundHeight:CGFloat = self.bounds.size.height
+        
+        self.backgroundView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: boundWith, height: boundHeight))
+        let img:UIImageView = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: (self.backgroundView?.frame.size.width)!, height: (self.backgroundView?.frame.size.height)!))
+        
+        self.backgroundView?.addSubview(img)
+        
+        self.searchButton = UIButton.init(frame: CGRect.init(x: kIconSize * 2, y: 0, width: boundWith - kIconSize*4, height: boundHeight))
+        
+        self.searchButton?.contentHorizontalAlignment = UIControlContentHorizontalAlignment.left
+        
+        let defaultFont:UIFont = UIFont.init(name: "Avenir Next", size: 14)
+        self.searchButton?.titleLabel?.font = defaultFont
+        self.searchButton?.setTitleColor(UIColor.lightGray, for: UIControlState.normal)
+        self.searchButton.s
+        
+        
+    }
+    
+    
+    
     
 
     
